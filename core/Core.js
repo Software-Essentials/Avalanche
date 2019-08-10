16724c5aab9cefdbd6f75192d08001120236cfc7
Lawrence Bensaid <lawrencebensaid@Juliens-MacBook-Air.local>
2019-08-10 10:10:55 +0200

    Updated Classes

---
+++ core/Core.js
@@ -0,0 +1,12 @@
+// General
+module.exports.Localisation = require("./Localisation.js");
+module.exports.Notification = require("./Notification.js");
+module.exports.Environment = require("./Environment.js");
+module.exports.Mailer = require("./Mailer.js");
+module.exports.Util = require("./Util.js");
+
+// Super components
+module.exports.ViewController = require("structure/ViewController.js");
+module.exports.Controller = require("structure/Controller.js");
+module.exports.Middleware = require("structure/Middleware.js");
+module.exports.Model = require("structure/Model.js");
