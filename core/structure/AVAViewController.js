bda3a3b728d94511da62f4a874db75123428f2af
Lawrence Bensaid <lawrencebensaid@Juliens-Air.therex.sites.plettnet.co.za>
2019-08-13 17:49:01 +0200

    Updated example

--- core/structure/AVAViewController.js
+++ core/structure/AVAViewController.js
@@ -14,15 +14,9 @@ class AVAViewController extends Controller {
         this.permission = null;
         this.layout = null;
         this.template = "viewControllerView";
-        this.tabs = [
-            {
-                name: "defaultTab",
-                title: "Empty",
-                active: true,
-                options: []
-            }
-        ];
+        this.tabs = [];
         this.modals = [];
+        this.variables = {};
         
         callback(this.willLoad, this);
         
@@ -35,41 +29,16 @@ class AVAViewController extends Controller {
 
         self.request = request;
         self.response = response;
+        
+        // const user = self.request.session.user;
+        const layout = typeof(self.layout) === "string" ? self.layout + ".layout.hbs" : "layout.hbs"
+        const template = self.template + ".hbs";
+        var vars = self.variables;
+        vars.layout = layout;
+        self.response.render(template, vars);
 
-        
-        if(self.permission && request.auth && !request.auth.permissions.includes(self.permission)) {
-            const layout = typeof(self.layout) === "string" ? self.layout + ".layout.hbs" : "layout.hbs"
-            self.response.status(403);
-            self.response.render("status/403.hbs", {
-                layout: layout
-            });
-            return;
-        }
-        
-        if(typeof(self.request.session.user) === "undefined") {
-            var rendererOptions = {};
-            const template = self.template + ".hbs";
-            rendererOptions.layout = typeof(self.layout) === "string" ? self.layout + ".layout.hbs" : "layout.hbs"
-            self.response.render(template, rendererOptions);
-            self.didLoad();
-            return;
-        } else {
-            const user = self.request.session.user;
-            const layout = typeof(self.layout) === "string" ? self.layout + ".layout.hbs" : "layout.hbs"
-            const template = self.template + ".hbs";
-            self.response.render(template, {
-                userCompany: user.company,
-                userFirstname: user.firstname,
-                userLastname: user.lastname,
-                userTitle: user.title,
-                layout: layout,
-                tabs: self.tabs,
-                modals: self.modals,
-                customModals: self.customModals
-            });
-            self.didLoad();
-            return;
-        }
+        self.didLoad();
+        return;
     }
 
     /**
