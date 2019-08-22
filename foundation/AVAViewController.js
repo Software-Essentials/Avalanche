4503609fe4618f9d3b29ccb0fe1530c08091d8ae
Lawrence Bensaid <lawrencebensaid@Juliens-MacBook-Air.local>
2019-08-22 17:02:04 +0200

    Added CSRF

--- foundation/AVAViewController.js
+++ foundation/AVAViewController.js
@@ -6,45 +6,47 @@ const AVAController = require("./AVAController");
  */
 class AVAViewController extends AVAController {
 
-    constructor(callback) {
-        super();
-        
-        this.permission = null;
-        this.layout = null;
-        this.template = "viewControllerView";
-        this.tabs = [];
-        this.modals = [];
-        this.variables = {};
-        
-        callback(this.willLoad, this);
+  constructor(callback) {
+    super();
+    
+    this.permission = null;
+    this.layout = null;
+    this.template = "viewControllerView";
+    this.tabs = [];
+    this.modals = [];
+    this.variables = {};
+    
+    callback(this.willLoad, this);
+  }
+
+
+  /**
+   * Fires when view is about to load
+   */
+  willLoad(request, response, self) {
+
+    self.request = request;
+    self.response = response;
+    
+    const layout = typeof(self.layout) === "string" ? self.layout + ".layout.hbs" : "layout.hbs"
+    const template = self.template + ".hbs";
+    var vars = self.variables;
+    vars.layout = layout;
+    if (environment.security.csrf) {
+      vars.CSRF = request.csrfToken();
     }
+    self.response.render(template, vars);
+
+    self.didLoad();
+    return;
+  }
 
 
-    /**
-     * Fires when view is about to load
-     */
-    willLoad(request, response, self) {
-
-        self.request = request;
-        self.response = response;
-        
-        const layout = typeof(self.layout) === "string" ? self.layout + ".layout.hbs" : "layout.hbs"
-        const template = self.template + ".hbs";
-        var vars = self.variables;
-        vars.layout = layout;
-        vars.CSRF = request.csrfToken();
-        self.response.render(template, vars);
-
-        self.didLoad();
-        return;
-    }
-
-
-    /**
-     * Fires when view has loaded
-     */
-    didLoad() {
-    }
+  /**
+   * Fires when view has loaded
+   */
+  didLoad() {
+  }
 
 }
 
