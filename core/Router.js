52319e28f0e0378096421ca793413f3515c2608b
Lawrence Bensaid <lawrencebensaid@85-117.dhcp.windesheim.nl>
2019-09-02 16:48:47 +0200

    Added feature so you can render files directly from routing

--- core/Router.js
+++ core/Router.js
@@ -45,6 +45,7 @@ class Router {
             const route = routes[i];
             const method = route.method;
             const routePath = route.path;
+            const routeFile = route.file;
             const routeMiddleware = route.middleware;
             const routePermission = route.permission;
             const controllerFile = route.controller;
@@ -59,37 +60,15 @@ class Router {
                 continue;
             }
             var routeHandler;
-            if(typeof(controllerHandler) === "string") {
-                // Controller handler
-                if (fs.existsSync(path.join(__dirname, `./controllers/${controllerFile}.js`))) {
-                    controller = require(`./controllers/${controllerFile}.js`);
-                } else {
-                    controller = require(`${projectPWD}/app/controllers/${controllerFile}.js`);
-                }
-                routeHandler = new controller();
-                if(typeof routeMiddleware === "object") {
-                    const filteredMiddlewareKeys = Object.keys(middleware).filter(function(i) {
-                        return routeMiddleware.includes(i);
-                    });
-                    var filteredMiddleware = [];
-                    for (let i = 0; i < filteredMiddlewareKeys.length; i++) {
-                        const key = filteredMiddlewareKeys[i];
-                        const mw = middleware[key];
-                        const mwo = new mw();
-                        filteredMiddleware[i] = (request, response, next) => { mwo.init(request, response, next); };
+            if (typeof(controllerFile) === "string") {
+                if(typeof(controllerHandler) === "string") {
+                    // Controller handler
+                    if (fs.existsSync(path.join(__dirname, `./controllers/${controllerFile}.js`))) {
+                        controller = require(`./controllers/${controllerFile}.js`);
+                    } else {
+                        controller = require(`${projectPWD}/app/controllers/${controllerFile}.js`);
                     }
-                    ExRouter[method.toLowerCase()](routePath, filteredMiddleware, routeHandler[controllerHandler]);
-                } else {
-                    ExRouter[method.toLowerCase()](routePath, routeHandler[controllerHandler]);
-                }
-            } else {
-                // ViewController handler
-                if (fs.existsSync(path.join(__dirname, `./views/${controllerFile}.js`))) {
-                    controller = require(`./views/${controllerFile}.js`);
-                } else {
-                    controller = require(`${projectPWD}/app/views/${controllerFile}.js`);
-                }
-                const cntrlr = new controller((routeHandler, that) => {
+                    routeHandler = new controller();
                     if(typeof routeMiddleware === "object") {
                         const filteredMiddlewareKeys = Object.keys(middleware).filter(function(i) {
                             return routeMiddleware.includes(i);
@@ -99,13 +78,44 @@ class Router {
                             const key = filteredMiddlewareKeys[i];
                             const mw = middleware[key];
                             const mwo = new mw();
-                            filteredMiddleware[i] = (request, response, next) => { mwo.init(request, response, next, cntrlr); };;
+                            filteredMiddleware[i] = (request, response, next) => { mwo.init(request, response, next); };
                         }
-                        ExRouter[method.toLowerCase()](routePath, filteredMiddleware, (request, response) => { routeHandler(request, response, that); });
+                        ExRouter[method.toLowerCase()](routePath, filteredMiddleware, routeHandler[controllerHandler]);
                     } else {
-                        ExRouter[method.toLowerCase()](routePath, (request, response) => { routeHandler(request, response, that); });
+                        ExRouter[method.toLowerCase()](routePath, routeHandler[controllerHandler]);
                     }
+                } else {
+                    // ViewController handler
+                    if (fs.existsSync(path.join(__dirname, `./views/${controllerFile}.js`))) {
+                        controller = require(`./views/${controllerFile}.js`);
+                    } else {
+                        controller = require(`${projectPWD}/app/views/${controllerFile}.js`);
+                    }
+                    const cntrlr = new controller((routeHandler, that) => {
+                        if(typeof routeMiddleware === "object") {
+                            const filteredMiddlewareKeys = Object.keys(middleware).filter(function(i) {
+                                return routeMiddleware.includes(i);
+                            });
+                            var filteredMiddleware = [];
+                            for (let i = 0; i < filteredMiddlewareKeys.length; i++) {
+                                const key = filteredMiddlewareKeys[i];
+                                const mw = middleware[key];
+                                const mwo = new mw();
+                                filteredMiddleware[i] = (request, response, next) => { mwo.init(request, response, next, cntrlr); };;
+                            }
+                            ExRouter[method.toLowerCase()](routePath, filteredMiddleware, (request, response) => { routeHandler(request, response, that); });
+                        } else {
+                            ExRouter[method.toLowerCase()](routePath, (request, response) => { routeHandler(request, response, that); });
+                        }
+                    });
+                }
+                continue;
+            }
+            if (typeof routeFile === "string") {
+                ExRouter.get(routePath, (request, response) => {
+                    response.render(routeFile);
                 });
+                continue;
             }
         }
         ExRouter.use((request, response) => {
@@ -114,7 +124,6 @@ class Router {
             response.render("status/404.hbs", {
                 layout: layout
             });
-            return;
         });
         return ExRouter;
     }
