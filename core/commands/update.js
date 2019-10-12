a944d69fdcf2dc8e1e13357cd377bf472f3d5b72
Lawrence Bensaid <lawrencebensaid@icloud.com>
2019-10-12 10:48:02 +0200

    Fixed update result message & Updated the animation

--- core/commands/update.js
+++ core/commands/update.js
@@ -1,5 +1,6 @@
 const CoreUtil = require("../CoreUtil");
 const { exec } = require("child_process");
+const fs = require("fs");
 
 
 /**
@@ -7,13 +8,21 @@ const { exec } = require("child_process");
  */
 function update() {
   const ready = typeof arguments[0] === "function" ? arguments[0] : () => {};
+
+  const pconfig = JSON.parse(fs.readFileSync(`${__dirname}/../../package.json`, "utf8"));
+  const latestVersion = pconfig.avalancheCache ? pconfig.avalancheCache.latestUpdate ? pconfig.avalancheCache.latestUpdate : null : null;
+  if (pconfig.version === latestVersion) {
+    process.stdout.write(`\n${CoreUtil.terminalPrefix()}\x1b[34m The Avalanche CLI is already up-to-date. (v${pconfig.version})\n\x1b[0m`);
+    ready();
+    return;
+  }
   process.stdout.clearLine();
   var i = 0, total = 10;
   const animation = setInterval(() => {
     process.stdout.clearLine();
     i = (i + 1) % total;
     const r = total - i;
-    var dots = "{ " + new Array(i + 1).join("=") + ">" + (new Array(r).join(" ")) + " [=] }";
+    var dots = "〈" + new Array(i + 1).join("◼︎") + (new Array(r).join(" ")) + "〉";
     process.stdout.write(`${CoreUtil.terminalPrefix()}\x1b[32m Updating the Avalanche CLI ${dots}\x1b[0m`)
     process.stdout.cursorTo(0);
   }, 50);
@@ -26,8 +35,12 @@ function update() {
   iProcess.on("exit", (code, signal) => {
     clearInterval(animation);
     process.stdout.clearLine();
-    const currentVersion = require(`${__dirname}/../../package.json`).version;
-    process.stdout.write(`\n${CoreUtil.terminalPrefix()}\x1b[32m The Avalanche CLI has been updated to the latest stable version (${currentVersion}).\n\x1b[0m`);
+    const pconfigAfter = JSON.parse(fs.readFileSync(`${__dirname}/../../package.json`, "utf8"));
+    if (pconfigAfter.version === latestVersion) {
+      process.stdout.write(`\n${CoreUtil.terminalPrefix()}\x1b[32m The Avalanche CLI has been updated to the latest stable version (v${pconfig.version} > v${pconfigAfter.version}).\n\x1b[0m`);
+    } else {
+      process.stdout.write(`\n${CoreUtil.terminalPrefix()}\x1b[31m Failed to update the Avalanche CLI! (This could be a permissions issue).\n\x1b[0m`);
+    }
     ready();
   });
 }
